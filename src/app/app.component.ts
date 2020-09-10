a15d2cfc0b3093185e5afe1277594b61b042e8cf
KiwiTech <you@example.com>
2020-09-10 11:38:43 +0530

    share buttons for facebook/twitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var FB;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social-share';
+
+  shareOverrideOGMeta(): void {
+    const overrideTitle = `${Math.random() * 10} Title`;
+    const overrideDescription = `${Math.random() * 10} Desc`;
+    const overrideLink = 'https://social-share-angular.herokuapp.com/';
+    const overrideImage = 'https://eventbox-prod.s3.amazonaws.com/profile/80245241589900869247.jpg';
+    FB.ui({
+      method: 'share_open_graph',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+        object: {
+          'og:url': window.location.href,
+          'og:title': overrideTitle,
+          'og:description': overrideDescription,
+          'og:image': overrideImage,
+          'og:image:width': 1279,
+          'og:image:height': 853
+        }
+      })
+    },
+       (response)=> {
+        console.log(response)
+        // Action after response
+      }, error=> console.log(error));
+  }
 }
