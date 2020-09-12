12885a566d9969d442e8a4c6be8f3f07a9041c36
KiwiTech <you@example.com>
2020-09-12 12:05:50 +0530

    dyanmic create twitter btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,19 @@ declare var twttr;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  twitterBtnCount = 0;
+  twitterBaseUrl = `https://twitter.com/intent/tweet?`;
+  hrefUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent('https://social-share-angular.herokuapp.com?overrideTitle=test&overrideDescription=testdesc&overrideImage=logo/9xFQUizaXWhZF6GsbO9bIGK7tGVXcHOfuZMPnOhf.png')}`;
   title = 'social-share';
-  tweetUrl =  'https://twitter.com/intent/tweet?text=Hello';
+  tweetUrl = 'https://twitter.com/intent/tweet?text=Hello';
   constructor(
     @Inject(DOCUMENT) private dom,
-    private readonly metaService: Meta, private readonly router: Router, private route: ActivatedRoute) { }
+    private readonly router: Router, private route: ActivatedRoute) { }
+
+  
   shareOverrideOGMeta(): void {
     const overrideTitle = `${Number(Math.random().toFixed(2)) * 100} Title`;
     const overrideDescription = `${Number(Math.random().toFixed(2)) * 100} Desc`;
-    const overrideLink = 'https://social-share-angular.herokuapp.com/';
     this.router.navigate(
       [],
       {
@@ -30,32 +34,26 @@ export class AppComponent {
         },
         queryParamsHandling: 'merge', // remove to replace all query params by provided
       }).then(res => {
-    FB.ui({
-      method: 'share_open_graph',
-      action_type: 'og.likes',
-      action_properties: JSON.stringify({
-        object: {
-          'og:url': this.dom.URL,
-          // 'og:title': overrideTitle,
-          // 'og:description': overrideDescription,
-          // // 'og:image': overrideImage,
-          // 'og:image:width': 1279,
-          // 'og:image:height': 853
-        }
-      })
-    },
-      (response) => {
-        console.log(response)
-        // Action after response
-      }, error => console.log(error));
-    });
+        FB.ui({
+          method: 'share_open_graph',
+          action_type: 'og.likes',
+          action_properties: JSON.stringify({
+            object: {
+              'og:url': this.dom.URL,
+            }
+          })
+        },
+          (response) => {
+            console.log(response)
+            // Action after response
+          }, error => console.log(error));
+      });
   }
 
-  tweet(){
-    const baseUrl = 'https://twitter.com/intent/tweet?text=Hello';
+  createTwitterBtn(): void {
     const overrideTitle = `${Number(Math.random().toFixed(2)) * 100} Title`;
     const overrideDescription = `${Number(Math.random().toFixed(2)) * 100} Desc`;
-    const overrideImage  = 'logo/9xFQUizaXWhZF6GsbO9bIGK7tGVXcHOfuZMPnOhf.png';
+    const overrideImage = 'logo/9xFQUizaXWhZF6GsbO9bIGK7tGVXcHOfuZMPnOhf.png';
     this.router.navigate(
       [],
       {
@@ -68,18 +66,35 @@ export class AppComponent {
         queryParamsHandling: 'merge', // remove to replace all query params by provided
       }).then(res => {
         this.tweetUrl = this.dom.URL;
+        const btnDiv: HTMLElement = document.createElement('div');
+        this.twitterBtnCount += 1;
+        const btnId = `new-btn-${this.twitterBtnCount}`;
+        btnDiv.setAttribute('id', btnId);
+        btnDiv.classList.add('btn-div');
+
+        document.getElementById('new-button-div').appendChild(btnDiv);
         twttr.widgets.createShareButton(
           this.tweetUrl,
-          document.getElementById('new-button'),
+          document.getElementById(btnId),
           {
             count: 'none',
             text: 'Sharing a URL using the Tweet Button'
-          }).then( (el)=> {
-            console.log("Button created.")
-            document.getElementById('new-button').click();
+          }).then((el) => {
+            // document.getElementById('new-button').click();
           });
-    });
+      });
   }
 
-  
+  generate(): void {
+    this.hrefUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(this.getUrl())}`;
+    this.dom.getElementById('twitter-share1').setAttribute('href', this.hrefUrl);
+    alert(`Url generated successfully you can click on tweet${this.hrefUrl}`);
+  }
+
+  getUrl(): string {
+    const overrideTitle = `${Number(Math.random().toFixed(2)) * 100} Title`;
+    const overrideDescription = `${Number(Math.random().toFixed(2)) * 100} Desc`;
+    const overrideImage = 'logo/9xFQUizaXWhZF6GsbO9bIGK7tGVXcHOfuZMPnOhf.png';
+    return `https://social-share-angular.herokuapp.com?overrideTitle=${overrideTitle}&overrideDescription=${overrideDescription}&overrideImage${overrideImage}`;
+  }
 }
